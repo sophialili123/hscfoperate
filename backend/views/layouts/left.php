41feaccab152eecacd04aa9e0508c0c6699127bd
Lily <sophia_lili123@163.com>
2017-08-30 13:21:56 +0800

    left

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -168,16 +168,16 @@ function initMenu($menuArray, $controllerName, $isSubUrl, $isShowIcon = false)
     <section class="sidebar">
 
         <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
-            </div>
-            <div class="pull-left info">
-                <p>Alexander Pierce</p>
-
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-        </div>
+<!--        <div class="user-panel">-->
+<!--            <div class="pull-left image">-->
+<!--                <img src="--><?//= $directoryAsset ?><!--/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>-->
+<!--            </div>-->
+<!--            <div class="pull-left info">-->
+<!--                <p>Alexander Pierce</p>-->
+<!---->
+<!--                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>-->
+<!--            </div>-->
+<!--        </div>-->
 
         <!-- search form -->
 <!--        <form action="#" method="get" class="sidebar-form">-->
