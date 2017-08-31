1c3ad53cbe42d6601a30d063ff9f865cfb18245d
Lily <sophia_lili123@163.com>
2017-08-31 17:52:09 +0800

    增加mongodb

---
+++ backend/controllers/MongodbController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LilyMM
+ * Date: 2017/8/31
+ * Time: 17:26
+ */
+
+namespace backend\controllers;
+
+
+class MongodbController
+{
+    //处理文件日志到mongodb
+    public function index(){
+
+    }
+
+}
